83d60fc509e4b9e855c7371ee5be4d6d6dbc1450
vamshi <vmarri92@gmail.com>
2025-03-28 22:40:37 +0530

    update changes

--- Backend/controllers/orderController.js
+++
@@ -1,9 +0,0 @@
-import orderModel from "../../../../RentalVerse/RentalVerse/backend/models/orderModel.js";
-import userModel from "../../../../RentalVerse/RentalVerse/backend/models/userModel.js"
-import stripe from "stripe"
-
-const placeOrder = async (req, res) => {
-
-}
-
-export { placeOrder }
--- Backend/routes/orderRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import authMiddleware from "../middleware/auth.js"
-import { placeOrder } from '../../../../MPR-sem6/PulmoCare/Backend/controllers/orderController.js'
-
-const orderRouter = express.Router();
-
-orderRouter.post("/place", authMiddleware, placeOrder);
-
-export default orderRouter
