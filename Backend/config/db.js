02ebc0d9de5ad2924456508d7a2330e29d21b258
vamshi <vmarri92@gmail.com>
2025-04-03 19:51:41 +0530

    remove clutter

--- Backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const ConnectDB = async () => {
-  await mongoose.connect('mongodb+srv://vam:Password123@cluster0.jionv.mongodb.net/Pulmocare').then(() => console.log("DB connected"))
-}
