b5c71426557dad4f9391900c52c239fb9296679b
vamshi <vmarri92@gmail.com>
2025-07-25 14:05:05 +0530

    check

--- Backend/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { loginUser, SignUser } from '../../../../MPR-sem6/PulmoCare/Backend/controllers/usercontroller.js'
-
-const userRouter = express.Router()
-
-userRouter.post('/SignUP', SignUser)// creating a endpoint
-userRouter.post('/Login', loginUser)// creating a endpoint
-
-export default userRouter;
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
-
 import Footer from './components/Footer'
 import { Outlet } from 'react-router'
 
